b90f4f8ee9b3c98af0745826e2d0b2c1ba2b7027
tmdgy6232 <tmdgy6232@naver.com>
2020-05-18 06:11:39 +0900

    3-1 오류코드 수정 및 챕터 완료 커밋

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -3,16 +3,24 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Mutation: {
     createAccount: async (_, args) => {
-      const { username, email, firstName = "", lastName = "", bio = "" } = args;
+      const {
+        username,
+        email,
+        firstName = "",
+        lastName = "",
+        bio = "",
+        loginSecret,
+      } = args;
       const user = await prisma.createUser({
         username,
         email,
         firstName,
         lastName,
-        bio
+        bio,
+        loginSecret,
       });
       console.log(user);
       return user;
-    }
-  }
+    },
+  },
 };
