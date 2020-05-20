977c2df187bc0bac30fba6a54b79180a19d67ca9
tmdgy6232 <tmdgy6232@naver.com>
2020-05-21 06:41:11 +0900

    3-3 SnedGrid => mailgun으로 api변경, 프로퍼티 입력작업 진행, 테스트 전 커밋

--- src/utils.js
+++ src/utils.js
@@ -3,31 +3,35 @@ import path from "path";
 dotenv.config({ path: path.resolve(__dirname, ".env") });
 import { adjectives, nouns } from "./word";
 import nodemailer from "nodemailer";
-import sgTransport from "nodemailer-sendgrid-transport";
+import mailgun from "nodemailer-mailgun-transport";
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
 };
 
-const sendMail = (email) => {
-  const options = {
+export const sendSecretMail = (adress, secret) => {
+  const DOMAIN = process.env.MAILGUN_DOMAIN;
+  const APIKEY = process.env.MAILGUN_APIKEY;
+  const option = {
+    apiKey: APIKEY,
+    domain: DOMAIN,
     auth: {
-      api_user: process.env.SENDGRID_USERNAME,
-      api_key: process.env.SENDGRID_PASSWORD,
+      apiKey: APIKEY,
+      domain: DOMAIN,
     },
   };
-  const client = nodemailer.createTransport(sgTransport(options));
-  return client.sendMail(email);
-};
-
-export const sendSecretMail = (adress, secret) => {
-  const email = {
+  const mg = mailgun(option);
+  const data = {
     from: "seulgi@prismagram.com",
     to: adress,
     subject: "Login Secret for Prismagram",
     html: `Hello! Your login secret it ${secret}. <br/> Copt paste on the app/website to log in`,
   };
-
-  return sendMail(email);
+  const sendMail = () => {
+    mg.messages().send(data, function(error, body) {
+      console.log(body);
+    });
+  };
+  return sendMail;
 };
